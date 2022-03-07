c4a11c721dd32e5f6f961cdf678c9affc8da071f
vndrewhk <drewhkm@gmail.com>
2022-03-07 17:27:30 -0500

    more styling for rank preview

---
+++ components/assets/icons/Hyper Roll Icons/HYPER_ICONS.js
@@ -0,0 +1,9 @@
+const HYPER_ICONS = {
+  grey: require("./grey.png").default,
+  green: require("./green.png").default,
+  blue: require("./blue.png").default,
+  purple: require("./purple.png").default,
+  hyper: require("./hyper.png").default,
+};
+
+export default HYPER_ICONS;
--- components/matches/HyperRollStats.js
+++ components/matches/HyperRollStats.js
@@ -1,11 +1,55 @@
+import Image from "next/image";
+import HYPER_ICONS from "../assets/icons/Hyper Roll Icons/HYPER_ICONS";
+import styles from "./HyperRollStats.module.css";
+
 const HyperRollStats = (props) => {
+  // const tiers = {
+  //   grey: 0,
+  //   green: 1400,
+  //   blue: 2600,
+  //   purple: 3400,
+  //   hyper: 4200,
+  // };
+
+  let playerTier;
+  if (props.matchInfo.ratedRating < 1400) {
+    playerTier = "grey";
+  } else if (props.matchInfo.ratedRating < 2600) {
+    playerTier = "green";
+  } else if (props.matchInfo.ratedRating < 3400) {
+    playerTier = "blue";
+  } else if (props.matchInfo.ratedRating < 4200) {
+    playerTier = "purple";
+  } else if (props.matchInfo.ratedRating >= 4200) {
+    playerTier = "hyper";
+  }
+
   return (
     <>
-   
-      <div>
+      <div className={styles["ranked-container"]}>
         <h2>Hyper Roll</h2>
+
+        <div className={styles["ranked-tier"]}>
+          <Image
+            src={HYPER_ICONS[playerTier]}
+            width={100}
+            height={100}
+            alt="hyper roll tier icon"
+            className={styles["ranked-icon"]}
+          ></Image>
+          <h3>Tier: {playerTier.toUpperCase()}</h3>
+        </div>
+
         <p>LP: {props.matchInfo.ratedRating}</p>
-        <p> Wins: {props.matchInfo.wins} Losses: {props.matchInfo.losses}</p>
+
+        <div className={styles["win-loss"]}>
+          <div>
+            <b>Wins</b>: {props.matchInfo.wins}
+          </div>
+          <div>
+            <b>Losses</b>:{props.matchInfo.losses}
+          </div>
+        </div>
       </div>
     </>
   );
